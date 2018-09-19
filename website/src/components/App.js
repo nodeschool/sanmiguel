927661267db52c0b8be257122e70a46253c6fbe2
D3Portillo <d3portillo@gmail.com>
2018-09-18 21:50:03 -0600

    final version

--- website/src/components/App.js
+++ website/src/components/App.js
@@ -4,12 +4,22 @@ import logo from "../assets/img/logo.svg"
 class App extends Component {
   render() {
     return (
-      <div style={{display: "flex", alignItems: "center", justifyContent: "center" , minHeight: "100vh"}}>
-        <img src={logo} alt="logo" style={{
-          display: "block",
-          maxWidth: "40rem",
-          padding: "2rem"
-        }}/>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh"
+        }}>
+        <img
+          src={logo}
+          alt="logo"
+          style={{
+            display: "block",
+            maxWidth: "40rem",
+            padding: "2rem"
+          }}
+        />
       </div>
     )
   }
