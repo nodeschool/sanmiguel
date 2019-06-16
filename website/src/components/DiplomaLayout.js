7788ad106dcf4c4225a9e539a99925d676d9886f
Denny <d3portillo@gmail.com>
2019-06-16 14:04:16 -0600

    adds topics from codevent1.0 This commit adds layout and topics from The Codevent

--- website/src/components/DiplomaLayout.js
+++ website/src/components/DiplomaLayout.js
@@ -28,49 +28,6 @@ Font.register({
     "https://cdn.staticaly.com/gh/D3Portillo/d3-assets/master/fonts/CamingoCode-Regular.ttf"
 })
 
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 30,
-    textAlign: "center",
-    fontFamily: "Roboto-bold",
-    width: "80%",
-    color: "#212121"
-  },
-  subtitle: {
-    margin: 12,
-    fontSize: 16,
-    textAlign: "center",
-    fontFamily: "Roboto",
-    width: "80%",
-    color: "#212121"
-  },
-  pre: {
-    fontFamily: "Code",
-    fontSize: 11,
-    textAlign: "center",
-    width: "80%",
-    color: "#212121"
-  },
-  image: {
-    position: "absolute",
-    height: "100%",
-    width: "100%"
-  },
-  sign: {
-    width: 70,
-    marginRight: -50
-  },
-  container: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "center",
-    paddingBottom: 30
-  }
-})
 const getDiplomaIMG = ({ isCodevent, version }) => {
   const data = layouts[isCodevent ? version : "default"]
   return !!data ? data : defaultIMG
@@ -85,6 +42,67 @@ export default memo(function({
   version = "1.0",
   onRender = () => console.log("pdf-rendered")
 }) {
+  const color = isCodevent ? "#FFFFFF" : "#212121"
+  const styles = StyleSheet.create({
+    title: {
+      fontSize: 30,
+      textAlign: "center",
+      fontFamily: "Roboto-bold",
+      width: "80%",
+      color
+    },
+    subtitle: {
+      margin: 12,
+      fontSize: 16,
+      textAlign: "center",
+      fontFamily: "Roboto",
+      width: "80%",
+      color
+    },
+    pre: {
+      normal: {
+        position: "absolute",
+        top: "40%",
+        width: "98%",
+        right: "47%",
+        transform: "rotate(-90deg)",
+        fontFamily: "Code",
+        fontSize: 9,
+        color: "black",
+        textAlign: "center"
+      },
+      codevent: {
+        position: "absolute",
+        top: "50%",
+        width: "100%",
+        left: "49.2%",
+        transform: "rotate(90deg)",
+        fontFamily: "Code",
+        fontSize: 9,
+        color: "black",
+        textAlign: "center"
+      }
+    },
+    image: {
+      position: "absolute",
+      height: "100%",
+      width: "100%"
+    },
+    sign: {
+      width: 70,
+      marginRight: -50
+    },
+    container: {
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      justifyContent: "center",
+      paddingBottom: 30
+    }
+  })
   const PDF = (
     <Document>
       <Page size="A4" orientation="landscape" wrap={false}>
@@ -93,24 +111,32 @@ export default memo(function({
           style={styles.image}
         />
         <View style={styles.container} fixed>
-          <Text style={styles.title}>La Comunidad de</Text>
-          <Text style={styles.title}>NodeSchool San Miguel</Text>
-          <Text style={styles.subtitle}>
-            OTORGA EL PRESENTE RECONOCIMIENTO A
+          <Text style={{ ...styles.title, fontSize: 40 }}>La Comunidad de</Text>
+          <Text style={{ ...styles.title, fontSize: 40 }}>
+            NodeSchool San Miguel
           </Text>
-          <Text style={{ ...styles.title, fontSize: 40 }}>{name}</Text>
           <Text style={styles.subtitle}>
-            POR HABER IMPARTIDO{" "}
-            {type === "taller"
-              ? `EL ${type.toUpperCase()}`
-              : `LA ${type.toUpperCase()}`}
+            Otorga el presente reconocimiento a:
+          </Text>
+          <Text style={styles.title}>{name}</Text>
+          {isCodevent && (
+            <Text style={{ ...styles.subtitle, marginBottom: -10 }}>
+              Por su excelente participación en el CODEVENT {version} cómo {""}
+              {type === "taller" ? `tallerista` : `ponente`}
+            </Text>
+          )}
+          <Text style={styles.subtitle}>
+            {isCodevent
+              ? "en el tema titulado:"
+              : "Por haber impartido el tema:"}
           </Text>
           <Text style={styles.title}>{tema}</Text>
           <Text style={styles.subtitle}>
-            El día {date} en la ciudad de San Miguel, El Salvador
+            El día {date} en la ciudad de San Miguel, El Salvador.
           </Text>
-          <Text style={styles.pre}>Verificable en:</Text>
-          <Text style={styles.pre}>{url}</Text>
+          <Text style={isCodevent ? styles.pre.codevent : styles.pre.normal}>
+            {url}
+          </Text>
         </View>
       </Page>
     </Document>
